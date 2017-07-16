847a25770ec9d7a53ca4abc13280bd09035c5616
geopopos <groros2@icloud.com>
2017-07-15 23:19:27 -0400

    Request html from page Parse html to get Course Number

--- parseHTML.py
+++ parseHTML.py
@@ -1,5 +1,20 @@
 import requests
 
+def getSubstring(string, index1, index2):
+    substring = ""
+    for i in range(index1, index2):
+        substring += string[i]
+    return substring
+
 page = requests.get('https://admin.washcoll.edu/schedules/17SPschedules.html')
 
-print(page.content)
+html = page.content
+
+courseIndex = html.find('k">AMS', 0, len(html))
+courseEnd = html.find("\n", courseIndex, len(html))
+courseString = getSubstring(html, courseIndex+3, courseEnd)
+    
+
+courseNumIndex = courseString.find(" ", 0, len(courseString))
+courseNumber = getSubstring(courseString, courseNumIndex+1, courseNumIndex+4)
+print(courseNumber)
